597e1bd1fe17b7efa32cda3703d6278228e39050
soidid <soidid@gmail.com>
2015-09-17 23:46:24 +0800

    (add) parse legislator's basic data:

---
+++ parseLegislator/parseLegislator.js
@@ -0,0 +1,77 @@
+
+var fs = require('fs')
+var csv = require('csv-parser')
+var clc = require('cli-color')
+var moment = require('moment')
+
+const START_ID = 1;
+
+function cht_to_eng(cht){
+	try{
+	switch(cht){
+		case '中國國民黨':
+			return 'KMT';
+		case '民主進步黨':
+			return 'DPP';
+		case '台聯':
+			return 'TSU';
+		case '親民黨':
+			return 'PFP';
+		case '民國黨':
+			return 'MKT';
+		case '無黨籍':
+			return 'NONE';
+		
+		default: 
+			throw new Error("Oh-Oh-找不到這個詞的英文捏！<o> "+cht);
+
+	}
+	}catch(e){
+		console.log(clc.red(e));
+		process.exit(1);
+	}
+}
+
+var Legislators = {};
+var Name2ID = {};
+var currentID = START_ID;
+
+fs.createReadStream('parseLegislator/data.csv')
+  .pipe(csv())
+  .on('data', function(data) {
+	  //console.log('row', data['議題名稱'])
+	  var record = {
+	  	id : currentID,
+	  	name : data['姓名'],
+	  	party : cht_to_eng(data['政黨']),
+	  	partyCht : data['政黨'],
+	  	gender : data['性別'],
+	  	age : data['年齡'],
+	  	constituency1 : data['第八屆選區1'],
+	  	constituency2 : data['第八屆選區2']
+	  }
+	  
+	  console.log(record);
+	  
+	  Legislators[currentID] = record;
+	  Name2ID[record.name] = currentID;
+
+	  currentID++;
+  })
+  .on('error', function (err)  { console.error('Error', err);})
+  .on('end',   function ()     { 
+  	  
+
+
+  	  fs.writeFile('parseLegislator/legislators.json', JSON.stringify(Legislators, null, 4), function (err) {
+  		if (err) return console.log(err);
+  		console.log(clc.bgGreen('legislators.json is saved.'));
+	  });
+	  
+	  fs.writeFile('parseLegislator/name2id.json', JSON.stringify(Name2ID, null, 4), function (err) {
+  		if (err) return console.log(err);
+  		console.log(clc.bgGreen('name2id.json is saved.'));
+	  });
+	  
+  });  
+
