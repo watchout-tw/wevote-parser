605a9319afee5e6106fad53b09abd969f3c7a539
soidid <soidid@gmail.com>
2015-10-26 12:33:01 +0800

    Parse 'hasResigned' info

--- parseLegislator/parseLegislator.js
+++ parseLegislator/parseLegislator.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 var csv = require('csv-parser')
 var clc = require('cli-color')
 var moment = require('moment')
+var cht2eng = require('../utils/cht2eng');
 
 const START_ID = 1;
 function yes_to_true(input){
@@ -24,31 +25,6 @@ function yes_to_true(input){
 		process.exit(1);
 	}
 }
-function cht_to_eng(cht){
-	try{
-	switch(cht){
-		case '中國國民黨':
-			return 'KMT';
-		case '民主進步黨':
-			return 'DPP';
-		case '台聯':
-			return 'TSU';
-		case '親民黨':
-			return 'PFP';
-		case '民國黨':
-			return 'MKT';
-		case '無黨籍':
-			return 'NONE';
-		
-		default: 
-			throw new Error("Oh-Oh-找不到這個詞的英文捏！<o> "+cht);
-
-	}
-	}catch(e){
-		console.log(clc.red(e));
-		process.exit(1);
-	}
-}
 
 
 var Legislators = {};
@@ -63,7 +39,7 @@ fs.createReadStream('parseLegislator/data.csv')
 	  var record = {
 	  	id : currentID,
 	  	name : data['姓名'],
-	  	party : cht_to_eng(data['政黨']),
+	  	party : cht2eng(data['政黨']),
 	  	partyCht : data['政黨'],
 	  	gender : data['性別'],
 	  	age : data['年齡'],
@@ -72,6 +48,7 @@ fs.createReadStream('parseLegislator/data.csv')
 	  	constituency2 : data['第八屆選區2'],
 
 	  	isCandidate : yes_to_true(data['是否為第九屆候選人']),
+	  	hasResigned : yes_to_true(data['已離職']),
 	  	candidateConstituency1 : data['第九屆選區1'],
 	  	candidateConstituency2 : data['第九屆選區2']
 	  }
